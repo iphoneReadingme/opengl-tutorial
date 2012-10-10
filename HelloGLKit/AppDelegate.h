297c5444dfff4128deca6a6515ebe811c83167b5
Jesse Armand <jesse@jessearmand.com>
2012-10-10 09:56:24 +0800

    Initial Commit

---
+++ HelloGLKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloGLKit
+//
+//  Created by Jesse Armand on 10/10/12.
+//  Copyright (c) 2012 Jesse Armand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloGLKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloGLKit
+//
+//  Created by Jesse Armand on 10/10/12.
+//  Copyright (c) 2012 Jesse Armand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
